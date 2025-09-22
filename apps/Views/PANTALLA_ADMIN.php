4fdb310a5b38e1e622d39563db489422351f8a4d
benjamin <benjaminmartello1509@gmail.com>
2025-09-21 21:23:58 -0300

    mensaje

--- apps/Views/PANTALLA_ADMIN.php
+++ apps/Views/PANTALLA_ADMIN.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Panel Administrador</title>
-        <link rel="stylesheet" href="../../public/CSS/style2.css">
+        <link rel="stylesheet" href="../../public/CSS/styleStart.css">
         
 </head>
 <body>
--- apps/Views/PANTALLA_CONTRATAR.php
+++ apps/Views/PANTALLA_CONTRATAR.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plataforma de Oficios</title>
-    <link rel="stylesheet" href="../../public/CSS/estilo.css">
+    <link rel="stylesheet" href="../../public/CSS/styleStart.css">
 </head>
 <body>
     <header class="header">
