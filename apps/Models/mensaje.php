e8e79b71e526492d30fd7bfbfa6026c152401cce
benjamin <benjaminmartello1509@gmail.com>
2025-09-30 06:05:32 -0300

    mensaje

--- apps/Models/mensaje.php
+++ apps/Models/mensaje.php
@@ -1,21 +1,6 @@
-	public function getIdMensaje() { return $this->IdMensaje; }
-
-	public function getContenido() { return $this->Contenido; }
-	public function setContenido($Contenido) { $this->Contenido = $Contenido; }
-
-	public function getFecha() { return $this->Fecha; }
-	public function setFecha($Fecha) { $this->Fecha = $Fecha; }
-
-	public function getEstado() { return $this->Estado; }
-	public function setEstado($Estado) { $this->Estado = $Estado; }
-
-	public function getIdUsuarioEmisor() { return $this->IdUsuarioEmisor; }
-	public function setIdUsuarioEmisor($IdUsuarioEmisor) { $this->IdUsuarioEmisor = $IdUsuarioEmisor; }
-
-	public function getIdUsuarioReceptor() { return $this->IdUsuarioReceptor; }
-	public function setIdUsuarioReceptor($IdUsuarioReceptor) { $this->IdUsuarioReceptor = $IdUsuarioReceptor; }
 <?php
-class mensaje {
+class mensaje
+{
 	public $IdMensaje;
 	public $Contenido;
 	public $Fecha;
@@ -23,12 +8,63 @@ class mensaje {
 	public $IdUsuarioEmisor;
 	public $IdUsuarioReceptor;
 
-	public function __construct($IdMensaje, $Contenido, $Fecha, $Estado, $IdUsuarioEmisor, $IdUsuarioReceptor) {
+	public function __construct($IdMensaje, $Contenido, $Fecha, $Estado, $IdUsuarioEmisor, $IdUsuarioReceptor)
+	{
 		$this->IdMensaje = $IdMensaje;
 		$this->Contenido = $Contenido;
 		$this->Fecha = $Fecha;
 		$this->Estado = $Estado;
 		$this->IdUsuarioEmisor = $IdUsuarioEmisor;
+		$this->IdUsuarioReceptor = $IdUsuarioReceptor;
+	}
+
+	public function getIdMensaje()
+	{
+		return $this->IdMensaje;
+	}
+
+	public function getContenido()
+	{
+		return $this->Contenido;
+	}
+	public function setContenido($Contenido)
+	{
+		$this->Contenido = $Contenido;
+	}
+
+	public function getFecha()
+	{
+		return $this->Fecha;
+	}
+	public function setFecha($Fecha)
+	{
+		$this->Fecha = $Fecha;
+	}
+
+	public function getEstado()
+	{
+		return $this->Estado;
+	}
+	public function setEstado($Estado)
+	{
+		$this->Estado = $Estado;
+	}
+
+	public function getIdUsuarioEmisor()
+	{
+		return $this->IdUsuarioEmisor;
+	}
+	public function setIdUsuarioEmisor($IdUsuarioEmisor)
+	{
+		$this->IdUsuarioEmisor = $IdUsuarioEmisor;
+	}
+
+	public function getIdUsuarioReceptor()
+	{
+		return $this->IdUsuarioReceptor;
+	}
+	public function setIdUsuarioReceptor($IdUsuarioReceptor)
+	{
 		$this->IdUsuarioReceptor = $IdUsuarioReceptor;
 	}
 }
